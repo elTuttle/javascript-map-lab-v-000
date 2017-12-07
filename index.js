b8f089f332a37dc5b16aa28918396d85cfcc9928
John Tuttle <thejwt@gmail.com>
2017-12-07 15:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const zebraStripes = [
 const sortedRobots = robots.map( function(robot) {
   return Object.assign({}, autobot, {
     alliance: if (knownDecepticons.includes(robot.name)) {
-      
+      "Decepticons"
     }
   });
 });
